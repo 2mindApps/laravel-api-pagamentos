0f9cb33f5e1813cab07adc92e332fb0a4e9c4028
Daniel Barcelos <daniel.barcelos@2mind.com.br>
2020-01-14 16:09:42 -0300

    First commit

---
+++ src/ApiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MindApps\LaravelApiPagamentos;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
